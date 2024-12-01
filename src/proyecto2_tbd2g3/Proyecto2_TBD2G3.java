35a3deec1e7c2df1bb0951eae1b39704c80b5d29
Diego Cruz <dfcm950@gmail.com>
2024-11-30 23:42:13 -0600

    Commit 1

---
+++ src/proyecto2_tbd2g3/Proyecto2_TBD2G3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2_tbd2g3;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class Proyecto2_TBD2G3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+    }
+    
+}
