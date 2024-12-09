1c960cc804e7ec7b64f48e10d0b1490ef3e9dde4
Junnior <junniorgsauceda@gmail.com>
2024-12-08 20:12:16 -0600

    Mostrando Tablas MyQSL

--- src/proyecto2_tbd2g3/ConexionMySQL.java
+++ src/proyecto2_tbd2g3/ConexionMySQL.java
@@ -4,9 +4,9 @@
  */
 package proyecto2_tbd2g3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,17 +16,49 @@ import javax.swing.JOptionPane;
 public class ConexionMySQL {
     static String url="jdbc:mysql://";
     static String urlOG="jdbc:mysql://tbd2g3.c1ci2a0qwno2.us-east-1.rds.amazonaws.com:3306/tbd2g3";
+    private static Connection connection=null;
     
-    public static Connection conectar(String Url,String User,String Password,String Port,String NombreDB){
-        Connection conexion=null;
+    public static boolean conectar(String Url,String User,String Password,String Port,String NombreDB){
+        boolean conectado=false;
         try {
             url="jdbc:mysql://"+Url+":"+Port+"/"+NombreDB;
-            conexion= DriverManager.getConnection(url, User, Password);
+            connection= DriverManager.getConnection(url, User, Password);
+            conectado=true;
             JOptionPane.showMessageDialog(null, "Se ha conectado correctamente con la Base de Datos");
         } catch (SQLException e) {
             e.printStackTrace();
+            conectado=false;
             JOptionPane.showMessageDialog(null, "Ha ocurrido un error con la conexion");
         }
-        return conexion;
+        return conectado;
+    }
+    
+    public List<String> getNombresTabla(String databaseName) {
+        List<String> tableNames = new ArrayList<>();
+        try {
+            // Verificar si hay conexión activa
+            if (connection == null || connection.isClosed()) {
+                throw new SQLException("No hay una conexión activa con la base de datos.");
+            }
+
+            // Consulta para obtener los nombres de las tablas
+            String sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = ? ORDER BY table_name";
+            
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                // Establecer el nombre de la base de datos en el parámetro
+                statement.setString(1, databaseName);
+
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    // Agregar los nombres de las tablas a la lista
+                    while (resultSet.next()) {
+                        tableNames.add(resultSet.getString("table_name"));
+                    }
+                }
+            }
+            System.out.println("Nombres de tablas obtenidos exitosamente.");
+        } catch (SQLException e) {
+            System.err.println("Error al obtener los nombres de las tablas: " + e.getMessage());
+        }
+        return tableNames;
     }
 }
